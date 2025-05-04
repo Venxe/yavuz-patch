1a0d1a15d9f6c8ffa5fad3a1cf44d4bad907d12b
Venxe <ivenxe@gmail.com>
2025-05-04 21:45:35 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -2,6 +2,10 @@
 
 CONF="$HOME/.config/hypr/hyprland.conf"
 
-sed -i 's/^monitor = DP-1,2560x1440@165\.08Hz,auto,1$/monitor = HDMI-1-A,1920x1080@60Hz,auto,1/' "$CONF"  
+sed -i '/^monitor = DP-1,2560x1440@165\.08Hz,auto,1$/{
+a\
+monitor = HDMI-1-A,1920x1080@60Hz,auto,1
+d
+}' "$CONF"
 
-echo "Satır başarıyla değiştirildi."
+echo "Eski DP-1 satırı silindi, HDMI-1-A satırı eklendi."
